56f599d85b1ed0a09c90fe1e045a72da41f84e8a
Alexander Alakhverdyants <alakhverdyants@yandex.ru>
2020-06-08 02:48:57 +0300

    parametric cube 2D plan

--- param_cube.cpp
+++ param_cube.cpp
@@ -3,3 +3,153 @@
 //
 
 #include "param_cube.h"
+#include <param_geom_node.h>
+#include <param_constraint_node.h>
+#include <param_sketch.h>
+
+#include <cur_arc.h>
+#include <solid.h>
+#include <action_solid.h>
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+AppParametricCubePlan::AppParametricCubePlan(const MbPlacement3D & place, double length, double width,
+                                             double holeRadius, double holeIndent)
+  : sketch(nullptr)
+  , controls()
+{
+  // Rectangle sides.
+  sketch = std::make_shared<AppParametricSketch>(place);
+  const MbCartPoint vertA(0., 0.), vertB(length, 0.), vertC(length, width), vertD(0., width);
+  auto lsegAB = _AddRectangleSide(vertA, vertB);
+  auto lsegBC = _AddRectangleSide(vertB, vertC);
+  auto lsegCD = _AddRectangleSide(vertC, vertD);
+  auto lsegDA = _AddRectangleSide(vertD, vertA);
+
+  // Setting constraints on rectangle elements.
+  const auto originPnt = sketch->GetControlPoint(*lsegAB, GCE_FIRST_END);
+  sketch->Fix(originPnt, GCE_FIX_GEOM);
+  controls["lengthX"] = sketch->Fix(*lsegAB, GCE_LENGTH);
+  controls["lengthY"] = sketch->Fix(*lsegDA, GCE_LENGTH);
+
+  sketch->ConnectSegments(*lsegAB, *lsegBC);
+  sketch->ConnectSegments(*lsegBC, *lsegCD);
+  sketch->ConnectSegments(*lsegCD, *lsegDA);
+  sketch->ConnectSegments(*lsegDA, *lsegAB);
+
+  sketch->Align(*lsegAB, GCE_HORIZONTAL);
+  sketch->Align(*lsegDA, GCE_VERTICAL);
+  sketch->Perpendicular(*lsegAB, *lsegBC);
+  sketch->Parallel(*lsegAB, *lsegCD);
+
+  // Create holes in each corner.
+  const double centerIndent = holeRadius + holeIndent;
+  SPtr<MbArc> circle(new MbArc(MbCartPoint(centerIndent, centerIndent), holeRadius));
+  auto holeA = _AddHoleInCorner(*circle, *lsegAB, *lsegDA, centerIndent);
+  controls["holeR"] = sketch->Fix(*holeA, GCE_RADIUS_DIM);
+
+  circle = new MbArc(MbCartPoint(length - centerIndent, centerIndent), holeRadius);
+  auto holeB = _AddHoleInCorner(*circle, *lsegAB, *lsegBC, centerIndent);
+  sketch->Equality(*holeA, *holeB, GCE_EQUAL_RADIUS);
+
+  circle = new MbArc(MbCartPoint(length - centerIndent, width - centerIndent), holeRadius);
+  auto holeC = _AddHoleInCorner(*circle, *lsegBC, *lsegCD, centerIndent);
+  sketch->Equality(*holeA, *holeC, GCE_EQUAL_RADIUS);
+
+  circle = new MbArc(MbCartPoint(centerIndent, width - centerIndent), holeRadius);
+  auto holeD = _AddHoleInCorner(*circle, *lsegCD, *lsegDA, centerIndent);
+  sketch->Equality(*holeA, *holeD, GCE_EQUAL_RADIUS);
+
+  Calculate();
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+AppParametricCubePlan::GeomNodePtr
+AppParametricCubePlan::_AddRectangleSide(const MbCartPoint & p1, const MbCartPoint & p2)
+{
+  SPtr<MbLineSegment> lseg(new MbLineSegment(p1, p2));
+  return sketch->AddLineSegment(*lseg);
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+AppParametricCubePlan::GeomNodePtr
+AppParametricCubePlan::_AddHoleInCorner(MbArc & circle, GeomNodeCRef side1, GeomNodeCRef side2, double indent)
+{
+  auto hole = sketch->AddCircle(circle);
+  auto center = sketch->GetControlPoint(*hole, GCE_CENTRE);
+  sketch->Distance(center, side1, indent);
+  sketch->Distance(center, side2, indent);
+  return hole;
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+bool AppParametricCubePlan::ChangeLengthX(double lenX)
+{
+  const GCE_result result = sketch->ChangeDimension(*controls["lenX"], lenX);
+  return OK(result) ? GCE_RESULT_Ok == Calculate() : false;
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+bool AppParametricCubePlan::ChangeLengthY(double lenY)
+{
+  const GCE_result result = sketch->ChangeDimension(*controls["lenY"], lenY);
+  return OK(result) ? GCE_RESULT_Ok == Calculate() : false;
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+bool AppParametricCubePlan::ChangeHoleRadius(double rad)
+{
+  const GCE_result result = sketch->ChangeDimension(*controls["holeR"], rad);
+  return OK(result) ? GCE_RESULT_Ok == Calculate() : false;
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+GCE_result AppParametricCubePlan::Calculate()
+{
+  return sketch->Evaluate();
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+AppParametricCube::AppParametricCube(const MbPlacement3D & place, double length, double width, double height)
+  : cube(nullptr)
+  , paramPlan(nullptr)
+{
+  paramPlan = std::make_shared<AppParametricCubePlan>(place, length, width, 5., 1.);
+
+  MbSolid * solid = nullptr;
+  MbSNameMaker names1(-1, MbSNameMaker::i_SideNone, 0);
+  SArray<MbCartPoint3D> points(4, 1);
+  points.push_back(place.GetOrigin());
+  points.push_back(place.GetOrigin() + place.GetAxisX()*length);
+  points.push_back(place.GetOrigin() + place.GetAxisY()*width);
+  points.push_back(place.GetOrigin() + place.GetAxisZ()*height);
+  ::ElementarySolid(points, et_Block, names1, solid);
+  cube = solid;
+  // mdlrWall = new MbInstance(*solid, place);
+}
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+void AppParametricCube::ChangeLengthX(double lenX)
+{
+  paramPlan->ChangeLengthX(lenX);
+  // тут запрашиваем новую длину
+
+  // тут перестраиваем кубик
+}
--- param_cube.h
+++ param_cube.h
@@ -5,7 +5,68 @@
 #ifndef PARAM_CUBE_H
 #define PARAM_CUBE_H
 
+#include <memory>
+#include <unordered_map>
+#include <templ_sptr.h>
+#include <gce_types.h>
+
+class MbPlacement3D;
+class MbCartPoint;
+class MbArc;
+class MbSolid;
+class AppGeomNode;
+class AppConstraintNode;
+class AppParametricSketch;
+
+//----------------------------------------------------------------------------------------
+//
+// ---
+class AppParametricCubePlan
+{
+  using PlanControls = std::unordered_map<std::string, std::shared_ptr<AppConstraintNode>>;
+  using GeomNodePtr  = std::shared_ptr<AppGeomNode>;
+  using GeomNodeCRef = const AppGeomNode &;
+
+  std::shared_ptr<AppParametricSketch> sketch;
+  PlanControls                         controls;
+
+public:
+  AppParametricCubePlan(const MbPlacement3D & place, double length, double width,
+                        double holeRadius, double holeIndent);
+
+public:
+  bool        ChangeLengthX(double lenX);
+  bool        ChangeLengthY(double lenY);
+  bool        ChangeHoleRadius(double rad);
+
+  GCE_result  Calculate();
+
+private:
+  GeomNodePtr _AddRectangleSide(const MbCartPoint &, const MbCartPoint &);
+  GeomNodePtr _AddHoleInCorner(MbArc & circle, GeomNodeCRef side1, GeomNodeCRef side2, double indent);
+
+public:
+  AppParametricCubePlan(const AppParametricCubePlan &) = delete;
+  AppParametricCubePlan & operator = (const AppParametricCubePlan &) = delete;
+};
+
+//----------------------------------------------------------------------------------------
+//
+// ---
 class AppParametricCube
-{};
+{
+  SPtr<MbSolid>                          cube;
+  std::shared_ptr<AppParametricCubePlan> paramPlan;
+
+public:
+  AppParametricCube(const MbPlacement3D & place, double length, double width, double height);
+
+public:
+  // void AddHole();
+  // void AddPocket();
+  void ChangeLengthX(double lenX);
+  void ChangeLengthY(double lenY);
+  void ChangeLengthZ(double lenZ);
+};
 
 #endif  // PARAM_CUBE_H
